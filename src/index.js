7e4646bc3604843d81e1af65978ed1db567e63ac
Thulio Philipe <thulioph@gmail.com>
2018-01-10 19:59:42 -0300

    First commit.

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// ====
+
+import './styles/index.css';
+import App from './containers/app';
+import registerServiceWorker from './registerServiceWorker';
+
+// ====
+
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+);
+
+registerServiceWorker();
