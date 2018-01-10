cc7afb995d9cfc275e232eb6e229f7efeec2f507
Thulio Philipe <thulioph@gmail.com>
2018-01-10 20:46:34 -0300

    Adds the initial components and containers.

---
+++ src/components/trackinfo/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// ====
+
+const TrackInfo = (props) => {
+    const { infoList, isVisible } = props;
+
+    let classe = `track-info`;
+
+    if (isVisible) {
+        classe = `track-info js-active`;
+    };
+
+    return (
+        <aside className={classe}>
+            <nav className="level is-mobile">
+                {infoList.map((el, idx) => (
+                    <div className="level-item has-text-centered" title={el.title} key={idx}>
+                        <div>
+                            <p className="heading">{el.legend}</p>
+                            <p className="title">{el.value}</p>
+                        </div>
+                    </div>
+                ))}
+            </nav>
+        </aside>
+    );
+};
+
+TrackInfo.propTypes = {
+    infoList: PropTypes.array,
+    isVisible: PropTypes.bool,
+};
+
+// ====
+
+export default TrackInfo;
