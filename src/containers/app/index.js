e19a6e856b13ffcdbdd2374aca4b13b9fdedcba3
Thulio Philipe <thulioph@gmail.com>
2018-01-12 15:13:53 -0300

    Updates the App container with new routes and didmount method.

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,31 +1,61 @@
-import React from 'react'
+import React from 'react';
+import { Redirect } from 'react-router';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Home from '../home';
 import Login from '../login';
+import Auth from '../auth';
+import Logout from '../logout';
 
 import Octocat from '../../components/octocat';
 
 // ====
 
 class App extends React.Component {
+  state = {
+    hasToken: false
+  };
+
+  componentDidMount() {
+    const access_token = localStorage.getItem('st_access_token');
+
+    if (access_token) {
+      console.warn('tem o token', access_token);
+      this.setState({ hasToken: true });
+    } else {
+      this.setState({ hasToken: false })
+    }
+  }
+
   render() {
+    const { hasToken } = this.state;
+
     return(
       <Router>
         <div>
-          <Octocat repoUrl="https://github.com/thulioph/spotify-tips/" title="Fork me on Github" />
+          {!hasToken ? <Redirect to="/login"></Redirect> : null}
           
+          <Octocat 
+            repoUrl="https://github.com/thulioph/spotify-tips/" 
+            title="Fork me on Github" 
+          />
+
           <ul>
             <li><Link to="/login">Login</Link></li>
-            <li><Link to="/">Home</Link></li>
-          </ul> 
+            {hasToken ? <li><Link to="/home">Home</Link></li> : null}
+            {hasToken ? <li><Link to="/logout">Logout</Link></li> : null}
+          </ul>
 
           <Route exact path='/' component={Home} />
           <Route path='/login' component={Login} />
+          <Route path='/auth' component={Auth} />
+          <Route path='/logout' component={Logout} />
         </div>
       </Router>
-    );
+    )
   }
-}
+};
+
+// ====
 
 export default App;
