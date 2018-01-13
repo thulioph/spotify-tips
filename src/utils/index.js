4afcada0793ebb63c9e6ae2aec63d169ddd9f195
thulioph <thulioph@gmail.com>
2018-01-13 00:24:11 -0300

    Adds a new method to create random strings.

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,15 @@
 export const getHashParams = (string, key) => {
     const matches = string.match(new RegExp(key + '=([^&]*)'));
     return matches ? matches[1] : null;
-}
+};
+
+export const randomString = (length) => {
+    const options = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let string = '';
+
+    for (let i = 0; i < length; i++) {
+        string += options.charAt(Math.floor(Math.random() * options.length));
+    }
+
+    return string;
+};
