c5f85d43904056ab78b7dfb9072277ea525496f6
thulioph <thulioph@gmail.com>
2018-01-17 00:53:37 -0300

    Adds the is-loading class on login button when is pressed.

--- src/containers/login/index.js
+++ src/containers/login/index.js
@@ -33,7 +33,9 @@ class Login extends React.Component {
         }
     }
 
-    requestLogin() {
+    requestLogin(evt) {
+        evt.currentTarget.classList.add('is-loading');
+        
         const client_id = encodeURIComponent(this.config.client_id);
         const scope = encodeURIComponent(this.config.scope);
         const redirect_uri = encodeURIComponent(this.config.redirect_uri);
@@ -52,18 +54,18 @@ class Login extends React.Component {
         }
 
         return (
-            <section className="section">
+            <section className="section section-login">
                 <div className="columns">
                     <div className="column is-half is-offset-one-quarter">
                         <aside className="card-wrapper">
                             <h1 className="title">Spotify Tips</h1>
                             <h2 className="subtitle">
-                                Obtenha <strong>métricas e dicas</strong> sobre o que você anda ouvindo.
+                                Conheça <strong>novas músicas e artistas</strong> com base nas que você mais gosta.
                             </h2>
 
                             <button className="button is-dark is-medium"
-                                onClick={this.requestLogin.bind(this)}>
-                                Login with Spotify
+                                onClick={(evt) => this.requestLogin(evt)}>
+                                Entrar com Spotify
                             </button>
                         </aside>
                     </div>
