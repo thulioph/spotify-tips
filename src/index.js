7facbd141cb59304d4f1f581f9bb4bf8df6bdee5
Thulio Philipe <thulioph@gmail.com>
2018-03-09 02:22:44 -0300

    Adds the dark theme.

--- src/index.js
+++ src/index.js
@@ -8,11 +8,14 @@ import AppComponent from 'containers/app';
 import registerServiceWorker from 'registerServiceWorker';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
 
 // ====
 
 const App = () => (
-    <MuiThemeProvider>
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
         <AppComponent />
     </MuiThemeProvider>
 );
