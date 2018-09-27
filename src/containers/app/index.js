fbe6c2e4df6208907d807f3c55390f5786c3e22b
Thulio Philipe <thulioph@gmail.com>
2018-09-27 18:49:31 -0300

    Updates the app container to manage the routes using public and private.

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,68 +1,43 @@
 import React from 'react';
-import { Redirect } from 'react-router';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Router, Switch } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 
-import Home from 'containers/home';
 import Login from 'containers/login';
+import Home from 'containers/home';
 import Auth from 'containers/auth';
 import Logout from 'containers/logout';
-
 import TrackInfo from 'containers/track-info';
+import Site from 'containers/site';
+
+import { PublicRoute, PrivateRoute } from 'routes';
 
 import Octocat from 'components/octocat';
 
-import Storage from 'utils/Storage';
-
-// ====
+const browserHistory = createBrowserHistory();
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      hasToken: false
-    };
-
-    this.storage = new Storage('spotify_tips');
-  }
-
-  componentWillMount() {
-    const access_token = this.storage.get();
-
-    if (access_token) {
-      this.setState({ hasToken: true });
-    } else {
-      this.setState({ hasToken: false })
-    }
-  }
-
   render() {
-    const { hasToken } = this.state;
-
     return(
-      <Router>
-        <main>
-          { !hasToken && <Redirect to='/login' /> } 
-          
-          <Octocat 
-            repoUrl="https://github.com/thulioph/spotify-tips/" 
-            title="Fork me on Github" 
-          />
+      <main>
+        <Octocat
+          title="Fork me on Github"
+          repoUrl="https://github.com/thulioph/spotify-tips/"
+        />
 
-          <Route exact path='/' render={() => <Redirect to='/login' />}/>
+        <Router history={browserHistory}>
+          <Switch>
+            <PublicRoute path='/' component={Site} exact />
+            <PublicRoute path='/login' component={Login} />
+            <PublicRoute path='/logout' component={Logout} />
 
-          <Route path='/home' component={Home}/>
-          <Route path='/track/:trackID/info' component={TrackInfo}/>
-
-          <Route path='/login' component={Login} />
-          <Route path='/auth' component={Auth} />
-          <Route path='/logout' component={Logout} />
-        </main>
-      </Router>
+            <PrivateRoute path='/auth' component={Auth} logged={false} />
+            <PrivateRoute path='/home' component={Home} logged={false} />
+            <PrivateRoute path='/track/:trackID/info' component={TrackInfo} logged={false} />
+          </Switch>
+        </Router>
+      </main>
     )
   }
 };
-
-// ====
 
 export default App;
