107b00abd9033232a96acfaaa5c65fc7e3087b3e
Thulio Philipe <thulioph@gmail.com>
2018-09-27 19:04:47 -0300

    Removes the unused code and import for css file.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// ====
-
-import 'styles/index.css';
 import AppComponent from 'containers/app';
 import registerServiceWorker from 'registerServiceWorker';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-
-
-// ====
 
 const App = () => (
     <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
