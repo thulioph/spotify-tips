3ce20aeb402f9de4e8337dac794cc4e9afa44144
Thulio Philipe <thulioph@gmail.com>
2018-09-27 19:22:03 -0300

    Updates the trackinfo component and create a new mdx file.

--- src/components/trackinfo/index.js
+++ src/components/trackinfo/index.js
@@ -1,38 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// ====
+const TrackInfo = ({ infoList, isVisible }) => {
+	let classe = `track-info`;
 
-const TrackInfo = (props) => {
-    const { infoList, isVisible } = props;
+	if (isVisible) {
+		classe = `track-info js-active`;
+	};
 
-    let classe = `track-info`;
-
-    if (isVisible) {
-        classe = `track-info js-active`;
-    };
-
-    return (
-        <aside className={classe}>
-            <nav className="level is-mobile">
-                {infoList && infoList.map((el, idx) => (
-                    <div className="level-item has-text-centered" title={el.title} key={idx}>
-                        <div>
-                            <p className="heading">{el.legend}</p>
-                            <p className="title">{el.value}</p>
-                        </div>
-                    </div>
-                ))}
-            </nav>
-        </aside>
-    );
+	return (
+		<aside className={classe}>
+			<nav className="level is-mobile">
+				{infoList && infoList.map((el, idx) => (
+					<div className="level-item has-text-centered" title={el.title} key={idx}>
+						<div>
+							<p className="heading">{el.legend}</p>
+							<p className="title">{el.value}</p>
+						</div>
+					</div>
+				))}
+			</nav>
+		</aside>
+	);
 };
 
 TrackInfo.propTypes = {
-    infoList: PropTypes.array,
-    isVisible: PropTypes.bool,
+	infoList: PropTypes.array,
+	isVisible: PropTypes.bool,
 };
 
-// ====
+TrackInfo.defaultProps = {
+	infoList: [],
+	isVisible: false,
+};
 
 export default TrackInfo;
