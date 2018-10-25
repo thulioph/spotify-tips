7b7dff925f1123891f397e610e6aa628bef438a2
Thulio Philipe <thulioph@gmail.com>
2018-10-25 18:04:06 -0300

    Updates the app container path on index.js file.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+
+import App from 'containers/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
