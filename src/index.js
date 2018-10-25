3237eef529d8e9641897b675d1b9e5034cb165fb
Thulio Philipe <thulioph@gmail.com>
2018-10-25 18:55:33 -0300

    Updates the index.js file to fixe the wrong file path.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './containers/App';
+
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-
-import App from 'containers/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
