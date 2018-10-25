a796b30ecb1bf08596236a13e4d5670aeb376f25
Thulio Philipe <thulioph@gmail.com>
2018-10-25 17:49:32 -0300

    Updates the index.js file with new routes.

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from '../../logo.svg';
-import '../../App.css';
+import React from 'react';
+import { Router, Switch, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import HomePage from 'pages/Home';
+import LoginPage from 'pages/Login';
+
+const browserHistory = createBrowserHistory();
+
+const App = () => {
+  return(
+    <Router history={browserHistory}>
+      <Switch>
+        <Route path='/' component={HomePage} exact />
+        <Route path='/login' component={LoginPage} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
